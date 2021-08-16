cf7b37bc5747849a12f5eb26e8082b0055de0c58
Akhil Patel <akhilsf@gmail.com>
2021-08-16 16:51:11 -0700

    minor efficiency improvement

--- server/schema.sql
+++ server/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS reviews (
   date TEXT NOT NULL,
   summary TEXT NOT NULL,
   body TEXT NOT NULL,
-  recommend BOOLEAN NOT NULL DEFAULT true,
+  recommend BOOLEAN NOT NULL,
   reported BOOLEAN NOT NULL DEFAULT false,
   reviewer_name TEXT NOT NULL,
   reviewer_email TEXT NOT NULL,
@@ -26,8 +26,23 @@ CREATE TABLE IF NOT EXISTS characteristics (
 );
 
 CREATE TABLE IF NOT EXISTS characteristic_reviews (
-  id SERIAL NOT NULL,
+  id SERIAL PRIMARY KEY,
   characteristic_id INT REFERENCES characteristics(id),
   review_id INT REFERENCES reviews(id),
   value INT NOT NULL
 );
+
+UPDATE reviews
+SET response = null
+WHERE response = 'null';
+
+CREATE INDEX rev_prod_id_idx ON reviews(product_id);
+CREATE INDEX rev_id_idx ON reviews_photos(review_id);
+CREATE INDEX char_prod_id_idx ON characteristics(product_id);
+CREATE INDEX char_id_idx ON characteristic_reviews(characteristic_id);
+
+-- need to re-run the below after loading data
+SELECT setval('reviews_id_seq', COALESCE((SELECT MAX(id)+1 FROM reviews), 1), false);
+SELECT setval('reviews_photos_id_seq', COALESCE((SELECT MAX(id)+1 FROM reviews_photos), 1), false);
+SELECT setval('characteristics_id_seq', COALESCE((SELECT MAX(id)+1 FROM characteristics), 1), false);
+SELECT setval('characteristic_reviews_id_seq', COALESCE((SELECT MAX(id)+1 FROM characteristic_reviews), 1), false);
