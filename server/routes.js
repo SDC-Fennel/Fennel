8174c4aae8207ea6a786fb914346e69686965b34
Akhil Patel <akhilsf@gmail.com>
2021-08-19 10:30:45 -0700

    loader.io setup

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const router = require('express').Router();
 const controllers = require('./controllers');
 
@@ -6,5 +7,9 @@ router.get('/reviews/meta', controllers.reviews.get);
 router.post('/reviews', controllers.reviews.post);
 router.put('/reviews/:review_id/helpful', controllers.reviews.put);
 router.put('/reviews/:review_id/report', controllers.reviews.put);
+
+router.get('/loaderio-f130cc16fb5189d0794586b567c1aa9c.txt', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '../loaderio-f130cc16fb5189d0794586b567c1aa9c.txt'));
+});
 
 module.exports = router;
